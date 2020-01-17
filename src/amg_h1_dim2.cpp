4671e72135545141f62fdfc8d977be8b8130ad36
Lukas <lukas.kogler@tuwien.ac.at>
2020-01-17 17:44:51 +0100

    check in vector h1 files

---
+++ src/amg_h1_dim2.cpp
@@ -0,0 +1,58 @@
+#define FILE_AMGH1_CPP
+#define FILE_AMGH1_CPP_DIM2
+
+#include "amg.hpp"
+
+#include "amg_factory.hpp"
+#include "amg_factory_nodal.hpp"
+#include "amg_factory_nodal_impl.hpp"
+#include "amg_factory_vertex.hpp"
+#include "amg_factory_vertex_impl.hpp"
+#include "amg_pc.hpp"
+#include "amg_energy.hpp"
+#include "amg_energy_impl.hpp"
+#include "amg_pc_vertex.hpp"
+#include "amg_pc_vertex_impl.hpp"
+#include "amg_h1.hpp"
+#include "amg_h1_impl.hpp"
+
+#define AMG_EXTERN_TEMPLATES
+#include "amg_tcs.hpp"
+#undef AMG_EXTERN_TEMPLATES
+
+
+namespace amg
+{
+
+  // extern template class Agglomerator<H1AMGFactory>;
+  extern template class SeqVWC<FlatTM>;
+  extern template class BlockVWC<H1Mesh>;
+  extern template class HierarchicVWC<H1Mesh>;
+  extern template class CoarseMap<H1Mesh>;
+  extern template class Agglomerator<H1Energy<2, double, double>, H1Mesh, H1Energy<2, double, double>::NEED_ROBUST>;
+  extern template class CtrMap<Vec<2,double>>;
+  extern template class GridContractMap<H1Mesh>;
+  extern template class VDiscardMap<H1Mesh>;
+
+  template class H1AMGFactory<2>;
+  template class VertexAMGPC<H1AMGFactory<2>>;
+
+  using PCC = VertexAMGPC<H1AMGFactory<2>>;
+  // using PCC = ElmatVAMG<H1AMGFactory<1>, double, double>;
+
+  // template class PCC;
+
+  RegisterPreconditioner<PCC> register_h1amg_2d ("ngs_amg.h1_dim2");
+
+} // namespace amg
+
+
+#include "python_amg.hpp"
+
+namespace amg
+{
+  void ExportH1Dim2 (py::module & m)
+  {
+    // ExportAMGClass<ElmatVAMG<H1AMGFactory<1>, double, double>>(m, "ngs_amg.h1_scal", "", [&](auto & m) { ; } );
+  };
+}
